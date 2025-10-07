a6d3811d414e54340600a02750e4751e41d849e5
Peaches <peachespear3@gmail.com>
2025-10-07 13:02:58 -0400

    Added support for json and tga files
    Added support for automatically renaming json and tga files if there is a demo file associated with them.

--- prefixer.go
+++ prefixer.go
@@ -38,53 +38,78 @@ func main() {
 
 	// For each file in directory...
 	mapCount := 0
-	for _, fileName := range files {
+	var prevFileName string
+	var mapName string
+	for _, fileFullName := range files {
 
-		// Only go through .dem files
-		if !strings.HasSuffix(fileName, ".dem") {
+		// Get file name, extension, and path information
+		extensionIndex := strings.LastIndex(fileFullName, ".")
+		if extensionIndex < 1 {
+			continue
+		}
+		extension := fileFullName[extensionIndex:]
+		fileName := fileFullName[:extensionIndex]
+		filePath := dirString + string(filepath.Separator) + fileFullName
+
+		// If json or tga, rename if tied to a demo
+		// If demo, then grab map name and all that jazz
+		switch extension {
+		case ".dem": // Demo file
+			// Open file
+			file, err := os.Open(filePath)
+			if err != nil {
+				fmt.Printf("Error opening %s: %s", filePath, err)
+				EnterToContinue()
+			}
+
+			// Read file and extract map name
+			scanner := bufio.NewScanner(file)
+			scanner.Scan()
+			rawText := string(scanner.Text()[mapStart : mapStart+mapLength])
+			for i := 0; i < len(rawText); i++ {
+				if !unicode.IsPrint(rune(rawText[i])) {
+					mapName = rawText[:i]
+					break
+				}
+			}
+			file.Close()
+
+			// Remove map version suffixes
+			suffixIndex := strings.LastIndex(mapName, "_")
+			if suffixIndex > 3 { // We skip when "suffix" is just the map prefix
+				mapTail := mapName[suffixIndex:]
+				for _, suffix := range mapSuffixes {
+					if strings.HasPrefix(mapTail, suffix) {
+						mapName = mapName[:suffixIndex]
+					}
+				}
+			}
+
+			// Update previous file name
+			prevFileName = fileName
+
+		case ".json": // json file
+			if fileName != prevFileName {
+				continue
+			}
+
+		case ".tga": // tga file
+			if fileName != prevFileName {
+				continue
+			}
+
+		default:
 			continue
 		}
 
-		// Open file
-		filePath := dirString + string(filepath.Separator) + fileName
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Printf("Error opening %s: %s", filePath, err)
-			EnterToContinue()
-		}
-
-		// Read file and extract map name
-		scanner := bufio.NewScanner(file)
-		scanner.Scan()
-		rawText := string(scanner.Text()[mapStart : mapStart+mapLength])
-		var mapName string
-		for i := 0; i < len(rawText); i++ {
-			if !unicode.IsPrint(rune(rawText[i])) {
-				mapName = rawText[:i]
-				break
-			}
-		}
-		file.Close()
-
-		// Remove map version suffixes
-		suffixIndex := strings.LastIndex(mapName, "_")
-		if suffixIndex > 3 { // We skip when "suffix" is just the map prefix
-			mapTail := mapName[suffixIndex:]
-			for _, suffix := range mapSuffixes {
-				if strings.HasPrefix(mapTail, suffix) {
-					mapName = mapName[:suffixIndex]
-				}
-			}
-		}
-
-		// If file name does not contain the map name, then replace existing index
-		if !strings.HasPrefix(fileName, mapName) {
-			dateIndex := strings.Index(fileName, "-") - 4 // prefixYYYY-MM-DD_HH-MM-SS
-			oldFileName := fileName
+		// If file name does not contain the map name, then replace existing prefix
+		if !strings.HasPrefix(fileFullName, mapName) {
+			dateIndex := strings.Index(fileFullName, "-") - 4 // prefixYYYY-MM-DD_HH-MM-SS
+			oldFileName := fileFullName
 			if dateIndex > 0 {
-				fileName = fileName[dateIndex:] // Remove prefix
+				fileFullName = fileFullName[dateIndex:] // Remove prefix
 			}
-			newPath := dirString + string(filepath.Separator) + mapName + "_" + fileName
+			newPath := dirString + string(filepath.Separator) + mapName + "_" + fileFullName
 			err := os.Rename(filePath, newPath)
 			if err != nil {
 				fmt.Printf("Error opening %s: %s", filePath, err)
@@ -96,12 +121,12 @@ func main() {
 	}
 
 	// Print total number of maps renamed
-	fmt.Printf("Renamed %d demos\n", mapCount)
+	fmt.Printf("Renamed %d files\n", mapCount)
 	EnterToContinue()
 }
 
 func EnterToContinue() {
 	fmt.Println("Press enter to exit...")
-	//fmt.Scanln()
+	fmt.Scanln()
 	os.Exit(0)
 }
