f76cdbf2952b5625fead8dcb31ed3472a337bffd
Peaches <peachespear3@gmail.com>
2025-10-07 01:44:11 -0400

    Updated to work based on parsed demo data

--- README.md
+++ README.md
@@ -1,58 +1,14 @@
 # Demo Prefixer
-A simple program designed to generate config files to automatically prefix demos with the name of the map they are recorded on. This is conceptually inspired by [this script on TFTV](https://www.teamfortress.tv/47180/demo-support-ds-prefix-on-any-map).  
-The program's main purpose is to be a simple-ish program for me to test and compare different programming languages, currently Odin and Go. When you download a release it will be compiled from the Odin code in the repository.
-
-### Important Notes: ###
-- You must use the Demo Support command (`ds_record`) to record demos for the prefix to apply!
-- Prefixes will not be applied to any maps you've downloaded after running the Demo Prefixer! You can set it to run automatically in various ways, see below.
-- The program will not apply prefixes to demos retroactively! It will only prefix demos recorded after you've run the program.
-- **Compatible with PeachREC.**
+A simple program designed to automatically replace the prefix of a recorded demo file with the map name the demo was recorded on.  
+The program's main purpose is to be a simple-ish program for me to test and compare different programming languages, currently Go.
 
 # Installation
 - Download the latest release from the [Releases page](https://github.com/PapaPeach/demo-prefixer/releases).
-- Put demo-prefixer.exe in your **tf/custom** folder.
+- Put demo-prefixer.exe in a folder containing demo (.dem) files you'd like to prefix.
 - Run demo-prefixer.exe and answer any prompts it gives you.
-- You'll see a new folder appear called **demo-prefixer** that will contain the generated config files.
-
-# Automated Running
-For convenience, the Demo Prefixer is designed to be able to be run with commandline arguments to allow for script integration.
-
-There are 4 accepted arguments to allow the program to be run without user interaction: 
-| Argument | Description |
-| :-- | :-- |
-| CompOnly | Only generates for Attack/Defend, Control Points, King of the Hill, or Payload. |
-| CompPlus | Generates like CompOnly with the addition of BBall, PASS Time, UltiDuo, and UltiTrio.<br>Will take priority over CompOnly if both are present. |
-| NoSuffix | Generates without map version suffixes (example_**a3**, example_**rc5**, etc).<br>Will keep event and other non-version number suffixes. |
-| Silent | Runs the program without prompting for user input for automation purposes.<br>All prompted options will assume a "No" response unless specified with an argument. |
-
-The easiest way to make use of these arguments for automatically running the program is with a batch script.  
-To set that up:
-- Create a text file (.txt) with any title. For example: `prefixed TF2.txt`.
-- Open your text file with **Notepad**.
-- Inside, copy and paste the following:
-```
-cd "[REPLACE WITH YOUR FILEPATH]\tf\custom"
-start demo-prefixer.exe silent [OTHER ARGUMENT] [ANOTHER ARGUMENT]
-start steam://rungameid/440
-exit
-```
-- Rename your text file to change the extension from `.txt` to `.bat`. For example: `prefixed TF2.bat`
-- Now running the batch script will run the Demo Prefixer and launch TF2 immediately after.
-### Explaination of the batch script:
-1. Navigates from where ever your batch file is to your custom folder.
-2. Starts the Demo Prefixer with the silent argument and any additional arguments you set.
-3. Starts TF2 through Steam, the same as if you launched via your Steam Library.
-4. Exits the terminal window that the batch script will open when it runs.
-
-There are other ways to schedule scripts to run on your computer. For example: on computer start, daily, weekly, etc. I'm not going to list those here for brevity and the previous method will cover any non-power-user cases.
 
 # How Does It Work?
-The program collects the maps from TF2's various map storage locations then filters maps according to user input / arguments. Once filters have been applied, the program generates config files for each remaining map with the name of the map formatted according to user input / arguments.
-
-On my computer this takes less than **30 milliseconds** to generate for roughly **300 maps**. I also tried make meaningful use of Odin's manual memory management to keep memory usage minimal, though likely unnecessary given the miniscule nature of the program.
 
 # Is This A Virus?
-### Nah. 
+### Nah.
 Your antivirus will likely warn you against running unknown executable files, which is reasonable advice. I'd encourage you to review the code in the repository for yourself and compile it from Source. While you're there, you can also tell me how bad my code is or how much I underutilized the languages!
-
-Both the Go and Odin code are meant to be as close to functionally identical as I could get the two languages, pick whichever one you prefer. Odin will run faster and hopefully more efficiently. Go is more well known, simpler, and you probably already have the Go compiler.
--- prefixer.go
+++ prefixer.go
@@ -2,274 +2,106 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
+	"unicode"
 )
 
-const WishDir = "tf" + string(filepath.Separator) + "custom"
-const CfgDir = "custom" + string(filepath.Separator) + "demo-prefixer" + string(filepath.Separator) + "cfg"
+const mapStart = 536
+const mapLength = 260
 
-var mapDirs = [3]string{"maps", "download" + string(filepath.Separator) + "maps", "workshop" + string(filepath.Separator) + "content" + string(filepath.Separator) + "maps"}
-var compGamemodes = []string{"cp", "koth", "pl"}
-var compPlusGamemodes = []string{"bball", "cp", "koth", "pass", "pl", "ultiduo", "ultitrio"}
 var mapSuffixes = [5]string{"_a", "_b", "_f", "_rc", "_v"}
 
 func main() {
-	// Check program location
-	currentDir := CheckDir()
-	tfDir := currentDir[:strings.LastIndex(currentDir, string(filepath.Separator))]
-	err := os.Chdir(tfDir)
+	// Open current directory
+	workingDir, err := os.Getwd()
 	if err != nil {
-		panic("Could not change working directory to tf")
+		fmt.Println("Error getting working directory:", err)
+		EnterToContinue()
+	}
+	dirString := workingDir
+	dir, err := os.Open(dirString)
+	if err != nil {
+		fmt.Println("Error opening directory:", err)
+		EnterToContinue()
+	}
+	defer dir.Close()
+
+	// Get list of files in current directory
+	files, err := dir.Readdirnames(0)
+	if err != nil {
+		fmt.Println("Error getting files in current directory:", err)
+		EnterToContinue()
 	}
 
-	if len(os.Args) > 1 {
-		fmt.Println("Commandline arguments:", os.Args[1:])
-	}
+	// For each file in directory...
+	mapCount := 0
+	for _, fileName := range files {
 
-	var hasCompOnly bool
-	var hasNoSuffix bool
-	var compOnly int
-	var noSuffix bool
-	var madeDir bool
-
-	// Iterate through different map directories
-	for _, dir := range mapDirs {
-		fmt.Printf("Searching tf/%v directory for maps...", dir)
-
-		var filteredMaps []string
-		mapsHandle, mhErr := os.Open(dir)
-		switch {
-		// If file doesn't exist, skip it
-		case errors.Is(mhErr, os.ErrNotExist):
+		// Only go through .dem files
+		if !strings.HasSuffix(fileName, ".dem") {
 			continue
-		// Unexpected error
-		case mhErr != nil:
-			fmt.Println(mhErr)
-			panic("Failed to convert maps path to handle")
-		}
-		defer mapsHandle.Close()
-
-		// Get contents of maps directory
-		maps, mErr := mapsHandle.Readdirnames(0)
-		if mErr != nil {
-			panic("Failed to get contents of tf/" + dir)
 		}
 
-		// Filter map names
-		for _, m := range maps {
-			// Remove file extension
-			if !strings.Contains(m, ".") {
-				continue
-			}
-			mapName := m[:strings.Index(m, ".")] //noinspection
-
-			// Only add maps
-			if strings.HasSuffix(m, ".bsp") {
-				filteredMaps = append(filteredMaps, mapName)
-			} else {
-				fmt.Printf("Skipping: %v, not a map.\n", m)
-			}
+		// Open file
+		filePath := dirString + string(filepath.Separator) + fileName
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Printf("Error opening %s: %s", filePath, err)
+			EnterToContinue()
 		}
 
-		// Customize map names based on user input / commandline args
-		if len(os.Args) > 1 { // Has commandline args
-			// CompOnly arg
-			if slices.Contains(os.Args[1:], "componly") || slices.Contains(os.Args[1:], "CompOnly") {
-				hasCompOnly = true
-				compOnly = 1
-			}
-			// CompPlus arg
-			if slices.Contains(os.Args[1:], "compplus") || slices.Contains(os.Args[1:], "CompPlus") {
-				hasCompOnly = true
-				compOnly = 2
-			}
-			// NoSuffix arg
-			if slices.Contains(os.Args[1:], "nosuffix") || slices.Contains(os.Args[1:], "NoSuffix") {
-				hasNoSuffix = true
-				noSuffix = true
-			}
-			// Silent arg
-			if slices.Contains(os.Args[1:], "silent") || slices.Contains(os.Args[1:], "Silent") {
-				hasCompOnly = true
-				hasNoSuffix = true
+		// Read file and extract map name
+		scanner := bufio.NewScanner(file)
+		scanner.Scan()
+		rawText := string(scanner.Text()[mapStart : mapStart+mapLength])
+		var mapName string
+		for i := 0; i < len(rawText); i++ {
+			if !unicode.IsPrint(rune(rawText[i])) {
+				mapName = rawText[:i]
+				break
 			}
 		}
-		for !hasCompOnly { // User input for CompOnly
-			fmt.Println("\nWould you like to generate prefixes only for competitive gamemodes (AD, CP, PL, KOTH)?\nYou may also generate for extended competitive gamemodes (bball, passtime, ultiduo, ultitrio)\n[Y] Yes\n[N] No\n[E] Extended")
+		file.Close()
 
-			reader := bufio.NewReader(os.Stdin)
-			response, rErr := reader.ReadString('\n')
-			if rErr != nil {
-				panic("Error getting user input for CompOnly")
-			}
-			response = strings.TrimSpace(response)
-			switch {
-			case strings.EqualFold(response, "y") || strings.EqualFold(response, "yes"):
-				hasCompOnly = true
-				compOnly = 1
-				fmt.Println("Generating map prefixes for competitive gamemodes only")
-			case strings.EqualFold(response, "e") || strings.EqualFold(response, "extended"):
-				hasCompOnly = true
-				compOnly = 2
-				fmt.Println("Generating map prefixes for extended competitive gamemodes")
-			case strings.EqualFold(response, "n") || strings.EqualFold(response, "no"):
-				hasCompOnly = true
-				compOnly = 0
-				fmt.Println("Generating map prefixes for all gamemodes")
-			}
-		}
-		for !hasNoSuffix { // User input for NoSuffix
-			fmt.Println("\nWould you like to remove suffixes from map name prefixes?\nExample: cp_process_f12 -> cp_process\n[Y] Yes\n[N] No")
-			reader := bufio.NewReader(os.Stdin)
-			response, rErr := reader.ReadString('\n')
-			if rErr != nil {
-				panic("Error getting user input for NoSuffix")
-			}
-			response = strings.TrimSpace(response)
-			switch {
-			case strings.EqualFold(response, "y") || strings.EqualFold(response, "yes"):
-				hasNoSuffix = true
-				noSuffix = true
-				fmt.Println("Generating map prefixes without suffixes")
-			case strings.EqualFold(response, "n") || strings.EqualFold(response, "no"):
-				hasNoSuffix = true
-				noSuffix = false
-				fmt.Println("Generating map prefixes with suffixes")
-			}
-		}
-
-		// Only apply prefix customizations if relevant
-		var mapNames []string
-		if compOnly != 0 || noSuffix {
-			filteredMaps, mapNames = CustomizeMaps(filteredMaps, compOnly, noSuffix)
-		} else {
-			mapNames = filteredMaps
-		}
-
-		// Generate tf/custom/demo-prefixer/cfg if not done yet
-		if !madeDir {
-			MakeCfgDir()
-			madeDir = true
-		}
-
-		WriteCfgs(filteredMaps, mapNames)
-
-		fmt.Println()
-	}
-}
-
-func CheckDir() string {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error checking working directory")
-	}
-
-	if !strings.HasSuffix(currentDir, WishDir) {
-		fmt.Printf("Program is currently in: \"%v\"\nPlease put program into your \"tf/custom\" folder.\n", currentDir)
-		PressToExit()
-	}
-	return currentDir
-}
-
-func CustomizeMaps(maps []string, compOnly int, noSuffix bool) ([]string, []string) {
-	var filteredMaps []string
-	switch compOnly {
-	case 1:
-		for _, m := range maps {
-			if !strings.Contains(m, "_") {
-				continue
-			}
-			prefix := m[:strings.Index(m, "_")]         //noinspection
-			if slices.Contains(compGamemodes, prefix) { // Add if comp gamemode
-				filteredMaps = append(filteredMaps, m)
-			}
-		}
-	case 2:
-		for _, m := range maps {
-			if !strings.Contains(m, "_") {
-				continue
-			}
-			prefix := m[:strings.Index(m, "_")]             //noinspection
-			if slices.Contains(compPlusGamemodes, prefix) { // Add if comp extended gamemode
-				filteredMaps = append(filteredMaps, m)
-			}
-		}
-	default:
-		filteredMaps = maps
-	}
-
-	var mapNames []string
-	if noSuffix {
-		for _, m := range filteredMaps {
-			var suffixFound bool
-			if strings.Count(m, "_") > 1 { // Check if prefix exists
-				suffix := m[strings.LastIndex(m, "_"):]
-				for _, s := range mapSuffixes { // If suffix is one that should be removed
-					if strings.HasPrefix(suffix, s) {
-						name := m[:strings.LastIndex(m, "_")]
-						suffixFound = true
-						mapNames = append(mapNames, name)
-						break
-					}
+		// Remove map version suffixes
+		suffixIndex := strings.LastIndex(mapName, "_")
+		if suffixIndex > 3 { // We skip when "suffix" is just the map prefix
+			mapTail := mapName[suffixIndex:]
+			for _, suffix := range mapSuffixes {
+				if strings.HasPrefix(mapTail, suffix) {
+					mapName = mapName[:suffixIndex]
 				}
 			}
-			if !suffixFound { // Suffix not removed
-				mapNames = append(mapNames, m)
+		}
+
+		// If file name does not contain the map name, then replace existing index
+		if !strings.HasPrefix(fileName, mapName) {
+			dateIndex := strings.Index(fileName, "-") - 4 // prefixYYYY-MM-DD_HH-MM-SS
+			oldFileName := fileName
+			if dateIndex > 0 {
+				fileName = fileName[dateIndex:] // Remove prefix
 			}
-		}
-	} else { // Set map names to their file name
-		mapNames = filteredMaps
-	}
-	return filteredMaps, mapNames
-}
-
-func MakeCfgDir() {
-	// Make tf/custom/demo-prefixer/cfg
-	err := os.MkdirAll(CfgDir, os.ModePerm)
-	if err != nil && !errors.Is(err, os.ErrExist) {
-		panic("Failed to make tf/custom/demo-prefixer/cfg")
-	}
-}
-
-func WriteCfgs(filteredMaps []string, mapNames []string) {
-	fmt.Printf(
-		"Starting generation with \"%v\", ending with \"%v\"\n",
-		filteredMaps[0],
-		filteredMaps[len(filteredMaps)-1],
-	)
-	for i, m := range filteredMaps {
-		// Create file
-		cfgFile := fmt.Sprintf("%v%v%v.cfg", CfgDir, string(filepath.Separator), m)
-		cfgHandle, err := os.Create(cfgFile)
-		if err != nil {
-			panic("Failed to get cfgHandle")
-		}
-		defer cfgHandle.Close()
-
-		// Generate contents
-		cfgContents := "ds_prefix " + mapNames[i]
-
-		// Write file
-		_, err = cfgHandle.WriteString(cfgContents)
-		if err != nil {
-			panic("Error writing cfg contents to file: " + cfgFile)
+			newPath := dirString + string(filepath.Separator) + mapName + "_" + fileName
+			err := os.Rename(filePath, newPath)
+			if err != nil {
+				fmt.Printf("Error opening %s: %s", filePath, err)
+				EnterToContinue()
+			}
+			fmt.Printf("Renamed: %s \tTo: %s\n", oldFileName, newPath[strings.LastIndex(newPath, string(filepath.Separator))+1:])
+			mapCount++
 		}
 	}
 
-	fmt.Printf("Generated %v config files", len(filteredMaps))
+	// Print total number of maps renamed
+	fmt.Printf("Renamed %d demos\n", mapCount)
+	EnterToContinue()
 }
 
-func PressToExit() {
-	fmt.Println("\nPress Enter to exit")
-	_, err := fmt.Scanln()
-	if err != nil {
-		// Don't care
-		os.Exit(0)
-	}
+func EnterToContinue() {
+	fmt.Println("Press enter to exit...")
+	//fmt.Scanln()
 	os.Exit(0)
 }
